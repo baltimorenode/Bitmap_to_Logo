758208fe0736a8c88b5a1f85e9ea1b45e51dcdc1
Christopher Lindsay <42160806+clindsa1@users.noreply.github.com>
2018-12-13 11:03:06 -0500

    Created Font_Convert.py
    just stubs for now

---
+++ Font_Convert.py
@@ -0,0 +1,15 @@
+from PIL import Image
+from PIL import ImageColor
+import sys
+
+#open an image file
+#convert to byte array 5 bytes wide comma
+#save to file
+
+#need to choose a row and column count for the image
+#8rows by 5cols is standard
+
+# for 5 columns
+	#get 8 rows
+	#convert to bits
+	
